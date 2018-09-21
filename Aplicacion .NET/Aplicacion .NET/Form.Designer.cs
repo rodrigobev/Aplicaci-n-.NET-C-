c992ceeee8922ca30f208c448f4f9459b96bcef2
rodrigobev <43462772+rodrigobev@users.noreply.github.com>
2018-09-21 00:56:13 -0500

    Add files via upload

---
+++ Aplicacion .NET/Aplicacion .NET/Form.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Aplicacion.NET
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputLabel.Location = new System.Drawing.Point(205, 114);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(372, 42);
+            this.inputLabel.TabIndex = 0;
+            this.inputLabel.Text = "Número de referencia";
+            // 
+            // button
+            // 
+            this.button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button.Location = new System.Drawing.Point(281, 257);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(215, 62);
+            this.button.TabIndex = 1;
+            this.button.Text = "CALCULAR";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.inputTextBox.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(158, 168);
+            this.inputTextBox.MaxLength = 19;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(465, 50);
+            this.inputTextBox.TabIndex = 3;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(205, 416);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(353, 42);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.Text = "Referencia completa";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.outputTextBox.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(158, 471);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(465, 50);
+            this.outputTextBox.TabIndex = 5;
+            this.outputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(781, 614);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.inputLabel);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aplicación .NET (C#)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.TextBox outputTextBox;
+    }
+}
+
---
+++ Aplicacion .NET/Aplicacion .NET/Form.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calculos;
+
+namespace Aplicacion.NET {
+    public partial class Form : System.Windows.Forms.Form {
+
+        calculos calc = new calculos();
+
+        public Form() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        // Verifica que la referencia esté compuesta únicamente por dígitos. 
+        private bool digitsOnly (string str) {
+
+            foreach (char c in str) {
+                if (c < '0' || c > '9') {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void button_Click(object sender, EventArgs e) {
+
+            string referencia = inputTextBox.Text;
+
+
+            if (digitsOnly(referencia) && (referencia.Length >= 1 && referencia.Length <= 19)) {
+                outputTextBox.Text = calc.calcular(referencia);
+            }
+            else {
+                inputTextBox.Text = "";
+                MessageBox.Show("Valor inválido. Favor de introducir una referencia de 1 a 19 dígitos.");
+            }
+
+        }
+    }
+}
